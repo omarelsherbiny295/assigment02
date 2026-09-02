152e835bec555dcca1fb1a6adaa4ac114fa7b765
Elsherbinyomarayman2004 <Elsherbinyomarayman2004@gmail.com>
2026-09-02 09:40:26 +0300

    question2

--- assigment02/Program.cs
+++ assigment02/Program.cs
@@ -5,18 +5,26 @@
         static void Main(string[] args)
         {
             #region ques.1
+            //Book book = new Book();
+            //book.title = "programming";
+            //book.pages = 100;
+
+            //object obj = book;
+            //Console.WriteLine(obj);
+
+            #endregion
+
+            #region question2
             Book book = new Book();
             book.title = "programming";
             book.pages = 100;
 
-            object obj = book;
-            Console.WriteLine(obj);
+            Console.WriteLine(book.ToString());
+            Console.WriteLine(book.GetHashCode());
+            Console.WriteLine(book.Equals(book));
+            Console.WriteLine(book.GetType());
 
             #endregion
-
-
-
-
         }
     }
 }
